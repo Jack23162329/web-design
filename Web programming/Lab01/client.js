80641638fbac33b6b19fce67fb722f772da200eb
Jack23162329 <94346382+Jack23162329@users.noreply.github.com>
2024-02-23 05:13:56 +0800

    Add files via upload

--- Web programming/Lab01/client.js
+++ Web programming/Lab01/client.js
@@ -6,14 +6,15 @@ window.onload = function(){
     var welcome = document.getElementById("welcomeview")
     var profile = document.getElementById("profileview")
     var token = localStorage.getItem("token")
+    
 
-    // if(token){
-    //     document.body.innerHTML = profile.innerHTML
-    // }
-    // else{
+    if(token){
+        document.body.innerHTML = profile.innerHTML
+        displaypage()
+        
+    }else{
         document.body.innerHTML = welcome.innerHTML
-    // }
-
+    }
     // var profile = document.getElementById("welcomeview")
     //code that is executed as the page is loaded.
     //You shall put your own custom code here.
@@ -21,7 +22,25 @@ window.onload = function(){
     // window.alert("Hello TDDD97!")
     
 }
-
+function attachTab(){
+    const tabs = document.querySelectorAll('[data-tab-target]')
+        const tabContents = document.querySelectorAll('[data-tab-content]')
+        
+        tabs.forEach(tab => {
+            tab.addEventListener('click', ()=>{
+                const target = document.querySelector(tab.dataset.tabTarget)
+                tabContents.forEach(tabContent => {
+                    tabContent.classList.remove('active')
+                 })
+                tabs.forEach(tab => {
+                    tab.classList.remove('active')
+                 })
+                tab.classList.add('active')
+                target.classList.add('active')
+            })
+        })
+        
+}
 function validation(formData){
     var password = formData.s_password.value
     var confirm_password = formData.c_password.value
@@ -75,7 +94,6 @@ function validation(formData){
 function signin(formData){
 
     event.preventDefault()
-    var welcome = document.getElementById("welcomeview")
     var profile = document.getElementById("profileview")
     var signup = document.getElementById("signup")
     var email = formData.email.value
@@ -86,81 +104,76 @@ function signin(formData){
 
         console.log(sign_in.data, sign_in.message)
         localStorage.setItem("token", sign_in.data)
-        
+        displaypage()
 
-        document.body.innerHTML = profile.innerHTML 
-        displayHometab();
-        const tabs = document.querySelectorAll('[data-tab-target]')
-        const tabContents = document.querySelectorAll('[data-tab-content]')
-        
-        tabs.forEach(tab => {
-            tab.addEventListener('click', ()=>{
-                const target = document.querySelector(tab.dataset.tabTarget)
-                tabContents.forEach(tabContent => {
-                    tabContent.classList.remove('active')
-                 })
-                tabs.forEach(tab => {
-                    tab.classList.remove('active')
-                 })
-                tab.classList.add('active')
-                target.classList.add('active')
-            })
-        })
-        
-
-        // var visibility = document.getElementById("icon")
-        // visibility.addEventListener("click", togglevisibility)
-        // function togglevisibility(){
-        //     const oldpassword = document.getElementById("oldpassword")
-        //     if (oldpassword.type === "password"){
-        //         oldpassword.type = "text"
-        //     }else{
-        //         oldpassword.type = "password"
-        //     }
-        // }
-        // var visibility_1 = document.getElementById("icon-2")
-        // visibility_1.addEventListener("click", togglevisibility_1)
-        // function togglevisibility_1(){
-        //     const oldpassword = document.getElementById("newpassword")
-        //     if (oldpassword.type === "password"){
-        //         oldpassword.type = "text"
-        //     }else{
-        //         oldpassword.type = "password"
-        //     }
-        // }
-        // var visibility_2 = document.getElementById("icon-3")
-        // visibility_2.addEventListener("click", togglevisibility_2)
-        // function togglevisibility_2(){
-        //     const oldpassword = document.getElementById("confirmpassword")
-        //     if (oldpassword.type === "password"){
-        //         oldpassword.type = "text"
-        //     }else{
-        //         oldpassword.type = "password"
-        //     }
-        // }
-        var bye_bye = document.getElementById("signout")
-        bye_bye.addEventListener("click", signout)
-        function signout(){
-
-            var token = localStorage.getItem("token")
-            var sign_out = serverstub.signOut(token)
-            if(sign_out.success === true){
-                document.body.innerHTML = welcome.innerHTML
-                message.textContent = sign_out.message
-            }else{
-                document.body.innerHTML = profile.innerHTML
-            }
-        
-        }
-        
-        
-        
     }
     else{
         signup.textContent = sign_in.message
         signup.style.color = "red"
     }
 }
+function displaypage(){
+    var profile = document.getElementById("profileview")
+    document.body.innerHTML = profile.innerHTML 
+    displayHometab()
+    displayaccountpage()
+    attachTab()
+}
+function displayaccountpage(){
+    
+    var visibility = document.getElementById("icon")
+    visibility.addEventListener("click", togglevisibility)
+    
+    var visibility_1 = document.getElementById("icon-2")
+    visibility_1.addEventListener("click", togglevisibility_1)
+    
+    var visibility_2 = document.getElementById("icon-3")
+    visibility_2.addEventListener("click", togglevisibility_2)
+    
+    var bye_bye = document.getElementById("signout")
+    bye_bye.addEventListener("click", signout)
+}
+
+function displayHometab(){
+    
+    var token = localStorage.getItem("token")
+    var userDataResponse = serverstub.getUserDataByToken(token)
+    
+    var Name = document.getElementById("Name")
+    var gender = document.getElementById("gender")
+    var city = document.getElementById("city")
+    var Country = document.getElementById("Country")
+    var Email = document.getElementById("Email")
+
+    if (userDataResponse.success === true){
+        
+        var userData = userDataResponse.data
+
+        Name.textContent = userData.firstname +" "+ userData.familyname
+        gender.textContent = userData.gender
+        city.textContent = userData.city
+        Country.textContent = userData.country
+        Email.textContent = userData.email
+
+    }else{
+        
+    }
+}
+
+function signout(){
+        var welcome = document.getElementById("welcomeview")
+        var token = localStorage.getItem("token")
+        localStorage.removeItem("token")
+        console.log(token)
+        var sign_out = serverstub.signOut(token)
+
+        if(sign_out.success === true){
+            document.body.innerHTML = welcome.innerHTML
+            message.textContent = sign_out.message
+        }else{
+            document.body.innerHTML = profile.innerHTML
+        }        
+    }
 
 function changePassword(formData){
     var oldPassword = formData.oldpassword.value
@@ -191,35 +204,50 @@ function changePassword(formData){
 
 
 }
-function displayHometab(){
-    
-    var token = localStorage.getItem("token")
-    var userDataResponse = serverstub.getUserDataByToken(token)
-    
-    var Name = document.getElementById("Name")
-    var gender = document.getElementById("gender")
-    var city = document.getElementById("city")
-    var Country = document.getElementById("Country")
-    var Email = document.getElementById("Email")
 
-    if (userDataResponse.success === true){
+function showmessage(formData){
+    var text = document.getElementById("textarea")
+    var output = document.getElementById("output_wall")
+    // var token = localStorage.getItem("token")
+
+    
+
+    // const Post = serverstub.postMessage(token, text)
+    
+    
+    // if (Post.success === true && text != ""){
+        console.log(text)
         
-        var userData = userDataResponse.data
-
-        Name.textContent = userData.firstname +" "+ userData.familyname
-        gender.textContent = userData.gender
-        city.textContent = userData.city
-        Country.textContent = userData.country
-        Email.textContent = userData.email
-
+        output.innerHTML = text.value + output.innerHTML
+        
+    // }
+    // else{
+    //     console.log('Failed to post message:',Post.message )
+    // }
+    
+}
+function togglevisibility(){
+    const oldpassword = document.getElementById("oldpassword")
+    if (oldpassword.type === "password"){
+        oldpassword.type = "text"
     }else{
-        
+        oldpassword.type = "password"
     }
 }
-function showmessage(){
-    const text = document.getElementById("textarea");
-    const output = document.getElementById("output_wall");
-
-    output.innerHTML = text.value;
+function togglevisibility_1(){
+    const oldpassword = document.getElementById("newpassword")
+    if (oldpassword.type === "password"){
+        oldpassword.type = "text"
+    }else{
+        oldpassword.type = "password"
+    }
+}
+function togglevisibility_2(){
+    const oldpassword = document.getElementById("confirmpassword")
+    if (oldpassword.type === "password"){
+        oldpassword.type = "text"
+    }else{
+        oldpassword.type = "password"
+    }
 }
 
